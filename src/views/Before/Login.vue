2221f84e7bfdc584eaf4db62dee39d3804ebe1fa
ajisyahroni <aji.0508@students.amikom.ac.id>
2019-07-31 11:15:10 +0700

    update ajik-fn

--- src/views/Before/Login.vue
+++ src/views/Before/Login.vue
@@ -3,10 +3,29 @@
 </template>
 
 <script>
-import a from 'axios'
+import a from "axios";
 export default {
-  methods:{
-    goLogin(){
+  mounted() {
+    this.goLogin();
+  },
+  data() {
+    return {
+      dataLogin: {
+        username: "sfelix_martins@hotmail.com",
+        password: "secret",
+        provider: "users"
+      }
+    };
+  },
+  methods: {
+    goLogin() {
+      a.post(
+        "http://10.200.179.166/geekcreation/public/api/login",
+        this.dataLogin
+      ).then(res => {
+        let { access_token } = res.data;
+        console.log(access_token);
+      });
     }
   }
 };
